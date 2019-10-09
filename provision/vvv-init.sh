c5947e8b7288aee9ab2f17799427c9c6f11cd1da
Jirayu Yingthawornsuk <me@jirayu.in.th>
2019-10-09 14:44:57 +0700

    change cert path / remove unused command

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -3,22 +3,12 @@
 
 echo " * Custom site template provisioner - downloads and installs a copy of WP stable for testing, building client sites, etc"
 
-# fetch the first host as the primary domain. If none is available, generate a default using the site name
-DOMAIN=`get_primary_host "${VVV_SITE_NAME}".test`
-SITE_TITLE=`get_config_value 'site_title' "${DOMAIN}"`
-WP_VERSION=`get_config_value 'wp_version' 'latest'`
-WP_LOCALE=`get_config_value 'locale' 'en_US'`
-WP_TYPE=`get_config_value 'wp_type' "single"`
-DB_NAME=`get_config_value 'db_name' "${VVV_SITE_NAME}"`
-DB_NAME=${DB_NAME//[\\\/\.\<\>\:\"\'\|\?\!\*]/}
-
 # Make a database, if we don't already have one
 echo -e "\nCreating database '${DB_NAME}' (if it's not already there)"
 mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`"
 echo -e "\nGranting the wp user priviledges to the '${DB_NAME}' database"
 mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO wp@localhost IDENTIFIED BY 'wp';"
 echo -e "\n DB operations done.\n\n"
-
 
 echo "Setting up the log subfolder for Nginx logs"
 noroot mkdir -p ${VVV_PATH_TO_SITE}/log
@@ -27,7 +17,7 @@ noroot touch ${VVV_PATH_TO_SITE}/log/nginx-access.log
 
 if [ -n "$(type -t is_utility_installed)" ] && [ "$(type -t is_utility_installed)" = function ] && `is_utility_installed core tls-ca`; then
   echo "Inserting the SSL key locations into the sites Nginx config"
-  VVV_CERT_DIR="/srv/certificates"
+  VVV_CERT_DIR="${VVV_PATH_TO_SITE}/certificates"
   # On VVV 2.x we don't have a /srv/certificates mount, so switch to /vagrant/certificates
   codename=$(lsb_release --codename | cut -f2)
   if [[ $codename == "trusty" ]]; then # VVV 2 uses Ubuntu 14 LTS trusty
@@ -62,19 +52,6 @@ END_HEREDOC
   sed -i -e "s|\(.*\){{LIVE_URL}}|\1${redirect_config}|" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
 else
   sed -i "s#{{LIVE_URL}}##" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
-fi
-
-get_config_value 'wpconfig_constants' |
-  while IFS='' read -r -d '' key &&
-        IFS='' read -r -d '' value; do
-      noroot wp config set "${key}" "${value}" --raw
-  done
-
-WP_PLUGINS=`get_config_value 'install_plugins' ''`
-if [ ! -z "${WP_PLUGINS}" ]; then
-    for plugin in ${WP_PLUGINS//- /$'\n'}; do
-        noroot wp plugin install "${plugin}" --activate
-    done
 fi
 
 echo "Site Template provisioner script completed"
